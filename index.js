12b2d390d2f67b909b2f5f964580036ba9aa7f1d
Tino <tinomuzambi@gmail.com>
2020-12-26 14:39:47 +0200

    getRules endpoint working.

--- index.js
+++ index.js
@@ -2,5 +2,30 @@ const needle = require("needle");
 const dotenv = require("dotenv").config();
 const TOKEN = process.env.TWITTER_TOKEN;
 
-const rulesURL = "";
-const streamURL = "";
+const rulesURL = "https://api.twitter.com/2/tweets/search/stream/rules";
+const streamURL =
+	"https://api.twitter.com/2/tweets/search/stream/?tweet.fields=public_metrics&expansions=author_id";
+
+const rules = [{ value: "giveaway" }];
+
+// Get stream rules
+const getRules = async () => {
+	const response = await needle("get", rulesURL, {
+		headers: {
+			Authorization: `Bearer ${TOKEN}`,
+		},
+	});
+
+	console.log(response.body);
+	return response.body;
+};
+
+(async () => {
+	let currentRules;
+	try {
+		currentRules = await getRules();
+	} catch (error) {
+		console.error(error);
+		process.exit(1);
+	}
+})();
